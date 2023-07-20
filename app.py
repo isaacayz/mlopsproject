75bc4ed4c9e1e60eb5adc400ee435892abebb09d
Isaac Ige <heywhydot16@gmail.com>
2023-07-20 22:45:18 +0100

    added html templates and app entry pint

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, render_template
+import pandas as pd
+import numpy as np
+
+from sklearn.preprocessing import StandardScaler
+
+app = Flask(__name__)
+
+
+# Route to homepage
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/predictdata', methods=['GET', 'POST'])
+def predict_datapoint():
+    if request.method == 'GET':
+        return render_template('home.html')
+    else:
+        pass
--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import numpy as np
 from src.exceptions import CustomException
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):
     try:
@@ -19,12 +20,18 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
     
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
 
             model.fit(X_train, y_train) 
 
@@ -38,5 +45,13 @@ def evaluate_models(X_train, y_train, X_test, y_test, models):
 
             return report
 
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+
+def load_object(self, file_path):
+    try:
+        with open(file_path, 'rb') as file_obj:
+            dill.load(file_obj)
     except Exception as e:
         raise CustomException(e, sys)
