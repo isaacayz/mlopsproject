a527e84a302975c210f9995cffd5b55b5d72296b
Isaac Ige <heywhydot16@gmail.com>
2023-07-20 23:46:25 +0100

    done with predict_pipeline module

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -29,7 +29,7 @@ class CustomData:
                 race_ethnicity: str,
                 parental_level_of_education,
                 lunch : str,
-                test_preparation_score: str,
+                test_preparation_course: str,
                 reading_score: str,
                 writing_score: str
                  ):
@@ -37,7 +37,7 @@ class CustomData:
         self.race_ethnicity = race_ethnicity
         self.parental_level_of_education = parental_level_of_education
         self.lunch = lunch
-        self.test_preparation_score = test_preparation_score
+        self.test_preparation_course = test_preparation_course
         self.reading_score = reading_score
         self.writing_score = writing_score
 
@@ -49,7 +49,7 @@ class CustomData:
                 'race_ethnicity' : [self.race_ethnicity],
                 'parental_level_of_education' : [self.parental_level_of_education],
                 'lunch' : [self.lunch],
-                'test_preparation_score' : [self.test_preparation_score],
+                'test_preparation_course' : [self.test_preparation_course],
                 'reading_score' : [self.reading_score],
                 'writing_score' : [self.writing_score]
             }
--- src/utils.py
+++ src/utils.py
@@ -49,9 +49,9 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, param):
         raise CustomException(e, sys)
     
 
-def load_object(self, file_path):
+def load_object(file_path):
     try:
         with open(file_path, 'rb') as file_obj:
-            dill.load(file_obj)
+            return dill.load(file_obj)
     except Exception as e:
         raise CustomException(e, sys)
