e585bb2ba859751cac6fd60e1efbec800a311355
Isaac Ige <heywhydot16@gmail.com>
2023-07-20 23:14:17 +0100

    added the predict method inside the predict pipeline

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -1,6 +1,7 @@
 import sys
 import pandas as pd
 from src.exceptions import CustomException
+from src.logger import logging
 from src.utils import load_object
 
 
@@ -8,6 +9,19 @@ class PredictPipeline:
     def __init__(self) -> None:
         pass
 
+    def predict(self, features):
+        try:
+            model_file_path = 'artifacts/model.pkl'
+            preprocessor_file_path = 'artifacts/preprocessor.pkl'
+            model = load_object(file_path=model_file_path)
+            preprocessor = load_object(file_path=preprocessor_file_path)
+            scaled_data = preprocessor.transform(features)
+            preds = model.predict(scaled_data)
+            logging.info('Done predicting from predict function')
+
+            return preds
+        except Exception as e:
+            raise CustomException(e, sys)
 
 class CustomData:
     def __init__(self,
@@ -26,3 +40,20 @@ class CustomData:
         self.test_preparation_score = test_preparation_score
         self.reading_score = reading_score
         self.writing_score = writing_score
+
+
+    def get_data_as_data_frame(self):
+        try:
+            custom_data_input_dict = {
+                'gender' : [self.gender],
+                'race_ethnicity' : [self.race_ethnicity],
+                'parental_level_of_education' : [self.parental_level_of_education],
+                'lunch' : [self.lunch],
+                'test_preparation_score' : [self.test_preparation_score],
+                'reading_score' : [self.reading_score],
+                'writing_score' : [self.writing_score]
+            }
+
+            return pd.DataFrame(custom_data_input_dict)
+        except Exception as e:
+            raise CustomException(e, sys)
